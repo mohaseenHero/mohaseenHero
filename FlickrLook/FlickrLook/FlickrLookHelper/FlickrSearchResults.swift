7a084386eb5d768869ecc40806d04024908b558e
mohaseenHero <87570149+mohaseenHero@users.noreply.github.com>
2021-07-17 17:44:22 +0530

    Add files via upload
    initial Commit

---
+++ FlickrLook/FlickrLook/FlickrLookHelper/FlickrSearchResults.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrSearchResults.swift
+//  FlickrLook
+//
+//  Created by Mohaseen TmmT on 17/07/21.
+//
+
+import Foundation
+
+struct FlickrSearchResults {
+    let searchTerm: String
+    let searchResults: [FlickrPhoto]
+}
---
+++ FlickrLook/FlickrLook/FlickrPhotoCell.swift
@@ -0,0 +1,12 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrLook
+//
+//  Created by Mohaseen TmmT on 17/07/21.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+}
